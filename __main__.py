76de7053c4923edabad6d4a6ae4525e8928881f7
RedSTAR.Cheng <74100786+RedSTARO@users.noreply.github.com>
2022-05-14 23:24:05 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -38,6 +38,7 @@ class Exp:
             self.coin(item['aid'])
         with open("log.log", "r", encoding="utf-8") as log:
             sendmsgtowx(log.read())
+            print(log.read())
     # 获取用户信息
     def getUserinfo(self):
         try:
