4af5b8aa84bbb9fe7145894cf3c2445a556d128f
RedSTAR.Cheng <74100786+RedSTARO@users.noreply.github.com>
2022-06-30 23:12:12 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -21,7 +21,7 @@ def start():
                     limit = json.load(f)["timeLimit"]
                 # 无限时长
                 if limit == "805c2ab1b4fc8e63f18917be231010c3":
-                 remainTimes = "无限制"
+                    remainTimes = "无限制"
                 # 31天
                 elif limit == "fa870bb9d2555e5cf35f39a2a1b2dd26": # fa870bb9d2555e5cf35f39a2a1b2dd26
                     start_time = datetime.datetime.strptime(f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}", '%Y-%m-%d-%H:%M')
@@ -32,18 +32,69 @@ def start():
                         remainTimes = str((end_time - now_time).days)
                     else:
                         continue
-            else:
-                logger.warning("Unknown time limit, ignore this config file, continue...")
-                continue
+                # 93
+                elif limit == "7e8e423c2bf5e4a08df35fb84d160406":
+                    start_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}", '%Y-%m-%d-%H:%M')
+                    end_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}",
+                        '%Y-%m-%d-%H:%M') + datetime.timedelta(days=93)
+                    now_time = datetime.datetime.now()
+                    # 判断当前时间是否在范围时间内
+                    if start_time < now_time < end_time:
+                        remainTimes = str((end_time - now_time).days)
+                    else:
+                        continue
+                # 186
+                elif limit == "a3544cc3a15af31d27c6663528f75e42":
+                    start_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}", '%Y-%m-%d-%H:%M')
+                    end_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}",
+                        '%Y-%m-%d-%H:%M') + datetime.timedelta(days=186)
+                    now_time = datetime.datetime.now()
+                    # 判断当前时间是否在范围时间内
+                    if start_time < now_time < end_time:
+                        remainTimes = str((end_time - now_time).days)
+                    else:
+                        continue
+                # 279
+                elif limit == "4a239d5aa84cabc4212e22108ea9b903":
+                    start_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}", '%Y-%m-%d-%H:%M')
+                    end_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}",
+                        '%Y-%m-%d-%H:%M') + datetime.timedelta(days=279)
+                    now_time = datetime.datetime.now()
+                    # 判断当前时间是否在范围时间内
+                    if start_time < now_time < end_time:
+                        remainTimes = str((end_time - now_time).days)
+                    else:
+                        continue
+                # 365
+                elif limit == "c84cd34010d3d75d26bd23b8f289b403":
+                    start_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}", '%Y-%m-%d-%H:%M')
+                    end_time = datetime.datetime.strptime(
+                        f"{startTime[0]}-{startTime[1]}-{startTime[2]}-{startTime[3]}:{startTime[4]}",
+                        '%Y-%m-%d-%H:%M') + datetime.timedelta(days=365)
+                    now_time = datetime.datetime.now()
+                    # 判断当前时间是否在范围时间内
+                    if start_time < now_time < end_time:
+                        remainTimes = str((end_time - now_time).days)
+                    else:
+                        continue
+                else:
+                    logger.warning("Unknown time limit, ignore this config file, continue...")
+                    continue
+
+                logger.info(f"剩余时间: {remainTimes}")
+                runner(fileList[j], f"{remainTimes}")
+                num += 1
+                logger.info(f"\n用户 {num} 签到完成\n")
         except:
             logger.warning("Config file running failed, ignore this config file, continue...")
             continue
-
-            logger.info(f"剩余时间: {remainTimes}")
-            runner(fileList[j], f"{remainTimes}")
-            num += 1
-            logger.info(f"\n用户 {num} 签到完成\n")
-
 start()
 # schedule.every().day.at("07:00:00").do(start)
 
